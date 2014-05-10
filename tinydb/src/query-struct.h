1caee5d4d76559e4097aaee60ef0f5e215cdb589
Roc Reguant <roc.reguant@gmail.com>
2014-05-10 15:15:37 +0200

    adding more structs

--- tinydb/src/query-struct.h
+++ tinydb/src/query-struct.h
@@ -3,8 +3,14 @@
 
 using namespace std;
 
+struct condition {
+	string comparator; 	
+	pair<string, string> l_attr; 
+	pair<string, string> > r_attr; 
+} ;
+
 struct query {
 	vector<string> select; 	//the selection names
-	vector<string> from; //only the table names
-	vector<pair<string, string> > where; //left argument equals (always as said in the example) right argument
+	vector<pair<string, string> > from; //only the table names <relation, binding>
+	vector<condition> where; 
 } ;
