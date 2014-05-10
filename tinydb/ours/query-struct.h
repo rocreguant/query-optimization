692a42d4ca12fb6bbca3cdfe835821d163523d4e
Roc Reguant <roc.reguant@gmail.com>
2014-05-10 14:27:09 +0200

    directory changes

---
+++ tinydb/ours/query-struct.h
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct query {
+	vector<string> select; 	//the selection names
+	vector<string> from; //only the table names
+	vector<pair<string, string> > where; //left argument equals (always as said in the example) right argument
+} ;
